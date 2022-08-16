f1498a5b5d7c5a5425edc61c5484d200d5436a15
zouyu <15079551143@163.com>
2022-08-17 00:30:20 +0800

    1.基础工程搭建

---
+++ service/src/main/java/com/zy/serviceApplication.java
@@ -0,0 +1,14 @@
+package com.zy;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class serviceApplication {
+    public static void main(String[] args) {
+        log.info("启动类启动");
+        SpringApplication.run(serviceApplication.class, args);
+    }
+}
