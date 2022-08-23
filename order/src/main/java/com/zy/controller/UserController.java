4e0514c8d51d1c30c9a76c6aff01740561275fc6
zouyu <15079551143@163.com>
2022-08-24 00:57:10 +0800

    1.使用nacos作为配置中心

--- order/src/main/java/com/zy/controller/UserController.java
+++ order/src/main/java/com/zy/controller/UserController.java
@@ -3,6 +3,8 @@ package com.zy.controller;
 import com.alibaba.fastjson.JSONObject;
 import com.zy.entity.User;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.CollectionUtils;
 import org.springframework.validation.BindingResult;
@@ -16,17 +18,22 @@ import java.util.List;
 
 @Controller
 @Slf4j
+@RefreshScope
 public class UserController {
+
+    @Value("${user.password}")
+    private String password;
 
     @RequestMapping(value = "/test", method = RequestMethod.POST)
     @ResponseBody
     public String test(@RequestBody  @Valid User user, BindingResult bindingResult) {
+        log.info("密码========={}",  password);
         StringBuffer sb = new StringBuffer();
         List<ObjectError> allErrors = bindingResult.getAllErrors();
         log.info("参数校验结果=========={}", JSONObject.toJSONString(allErrors));
         if(!CollectionUtils.isEmpty(allErrors)) {
             return allErrors.get(0).getDefaultMessage();
         }
-        return user.getName();
+        return password;
     }
 }
