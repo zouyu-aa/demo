c6f7ada82da09e33e4174353e9287377c47e083e
zouyu <15079551143@163.com>
2022-09-22 00:08:30 +0800

    1.引入feign

--- order/src/main/java/com/zy/OrderStarter.java
+++ order/src/main/java/com/zy/OrderStarter.java
@@ -3,9 +3,13 @@ package com.zy;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @Slf4j
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages = "com.zy")
 public class OrderStarter {
     public static void main(String[] args) {
         log.info("启动类启动");
--- service/src/main/java/com/zy/service/OrderService.java
+++ service/src/main/java/com/zy/service/OrderService.java
@@ -3,7 +3,8 @@ package com.zy.service;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name = "order", url = "http://localhost:8089/order")
+//@FeignClient(name = "order", url = "http://localhost:8089/order")
+@FeignClient(name = "order")
 public interface OrderService {
 
 
